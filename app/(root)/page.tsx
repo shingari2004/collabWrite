012ac5e7e6170a5de3ecca567ac0b2830eb31dc1
shingari2004 <129884376+shingari2004@users.noreply.github.com>
2025-07-13 14:33:35 +0530

    Update page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,6 @@ import AddDocumentBtn from '@/components/AddDocumentBtn';
 import { DeleteModal } from '@/components/DeleteModal';
 import Header from '@/components/Header'
 import Notifications from '@/components/Notifications';
-import { Button } from '@/components/ui/button'
 import { getDocuments } from '@/lib/actions/room.actions';
 import { dateConverter } from '@/lib/utils';
 import { SignedIn, UserButton } from '@clerk/nextjs'
@@ -38,7 +37,7 @@ const Home = async () => {
             />
           </div>
           <ul className="document-ul">
-            {roomDocuments?.data.map(({ id, metadata, createdAt }: any) => (
+            {roomDocuments?.data.map(({ id, metadata, createdAt }: unknown) => (
               <li key={id} className="document-list-item">
                 <Link href={`/documents/${id}`} className="flex flex-1 items-center gap-4">
                   <div className="hidden rounded-md bg-dark-500 p-2 sm:block">
