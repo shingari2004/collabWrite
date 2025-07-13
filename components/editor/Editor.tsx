0be25805506f8080f4229907bb301e643d48743a
shingari2004 <129884376+shingari2004@users.noreply.github.com>
2025-07-13 14:40:39 +0530

    Update Editor.tsx

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -30,6 +30,7 @@ function Placeholder() {
 export function Editor({ roomId, currentUserType }: { roomId: string, currentUserType: UserType }) {
   const status = useEditorStatus();
   const { threads } = useThreads();
+  console.log(roomId);
 
   const initialConfig = liveblocksConfig({
     namespace: 'Editor',
